434661efd0a825bedaa46046427d651599b93696
AfcharidesB <1931568@cgmatane.qc.ca>
2022-02-07 16:35:10 -0500

    Add theme files

---
+++ footer.php
@@ -0,0 +1,13 @@
+</main>
+    <footer class="flex v-aligne">
+        <div class="conteneur">
+            <p><?php bloginfo( 'name' )?>, tous droit réservées &copy <?php echo(date('Y'))?></p>
+        </div>
+
+    </footer>
+<?php wp_footer( );?>
+</body>
+</html>
+
+
+
